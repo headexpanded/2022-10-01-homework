7a6a58058e48345955582e015cfa1fa0dde03678
moncon <monconish@protonmail.com>
2022-10-03 10:08:23 +0200

    purified function builder

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 /**
  *
- * A list of AFL clubs,  which can be sorted alphabetically.
+ * A list of AFL clubs,
+ * which can be sorted alphabetically,
+ * and by the year of their debut in the league.
  *
  */
-
-const uList = document.body.querySelector("ul");
 
 let clubs = [];
 
@@ -16,66 +16,45 @@ const fetcher = fetch("https://api.squiggle.com.au/?q=teams")
   })
   .then((teams) => {
     clubs = teams.teams;
-    for (let index = 0; index < clubs.length; index++) {
-      const club = clubs[index];
-
-      const li = document.createElement("li");
-      uList?.appendChild(li);
-      li.className = "list-group-item";
-      const row = document.createElement("div");
-      row.id = "clubRow";
-      (row.className = "row"), "align-items-left";
-      const clubNameDiv = document.createElement("div");
-      clubNameDiv.id = "clubNameDiv";
-      (clubNameDiv.className = "col-6"), "clubNameDiv";
-      li.appendChild(row);
-      row.appendChild(clubNameDiv);
-      clubNameDiv.textContent = `${club.name}`;
-      const clubDebut = document.createElement("div");
-      clubDebut.id = "clubDebut";
-      clubDebut.className = "col-6";
-      row.appendChild(clubDebut);
-      clubDebut.textContent = `${club.debut}`;
-    }
+    builder(clubs);
   });
 
 // user sorts by name
 const nameSort = document.body.querySelector("#sortByName");
 nameSort?.addEventListener("click", () => {
-  const listItems = uList.querySelectorAll("#clubRow");
+  console.log(clubs[0].id);
   clubs = clubs.reverse();
-
-  for (let index = 0; index < listItems.length; index++) {
-    const listItem1 = listItems[index].firstChild;
-    const listItem2 = listItems[index].lastChild;
-    listItem1.textContent = `${clubs[index].name}`;
-    listItem2.textContent = `${clubs[index].debut}`;
-  }
+  const goAway = document.body.querySelector("li");
+  goAway?.parentElement.removeChild(goAway);
+  builder(clubs);
 });
 
 // user sorts by debut
 const debutSort = document.body.querySelector("#sortByDebut");
-debutSort?.addEventListener("click", () => {
-  const listItems = uList.querySelectorAll("#clubRow");
+debutSort?.addEventListener("click", () => {});
 
-  for (let index = 0; index < listItems.length; index++) {
-    const listItem1 = listItems[index].firstChild;
-    const listItem2 = listItems[index].lastChild;
-
-    if (listItem2.textContent > 2000) {
-      let oldClubs = clubs.sort((a, b) =>
-        a.debut < b.debut ? -1 : b.debut > a.debut ? 1 : 0
-      );
-      listItem1.textContent = `${oldClubs[index].name}`;
-      listItem2.textContent = `${oldClubs[index].debut}`;
-    } else {
-      let newClubs = clubs.sort((a, b) =>
-        a.debut > b.debut ? -1 : b.debut < a.debut ? 1 : 0
-      );
-      listItem1.textContent = `${newClubs[index].name}`;
-      listItem2.textContent = `${newClubs[index].debut}`;
-    }
+const builder = function (clubs) {
+  for (let index = 0; index < clubs.length; index++) {
+    const club = clubs[index];
+    const uList = document.body.querySelector("ul");
+    const li = document.createElement("li");
+    const row = document.createElement("div");
+    const clubNameDiv = document.createElement("div");
+    const clubDebut = document.createElement("div");
+    li.className = "list-group-item";
+    row.id = "clubRow";
+    clubNameDiv.id = "clubNameDiv";
+    clubDebut.id = "clubDebut";
+    (row.className = "row"), "align-items-left";
+    (clubNameDiv.className = "col-6"), "clubNameDiv";
+    (clubDebut.className = "col-6"), "clubDebutDiv";
+    uList?.appendChild(li);
+    li.appendChild(row);
+    row.appendChild(clubNameDiv);
+    row.appendChild(clubDebut);
+    clubNameDiv.textContent = club.name;
+    clubDebut.textContent = club.debut;
   }
-});
+};
 
 // END
